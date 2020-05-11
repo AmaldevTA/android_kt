5722de3ae952560226f96324c282acc5d2224d89
Amaldev <amaldev.ta@waypoint-systems.com>
2020-05-11 13:57:08 +0530

    Bluetooth Print

--- BluetoothPrint/app/src/main/java/com/tst/bluetoothprint/MainActivity.kt
+++ BluetoothPrint/app/src/main/java/com/tst/bluetoothprint/MainActivity.kt
@@ -2,18 +2,59 @@ package com.tst.bluetoothprint
 
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.tst.bluetoothprint.databinding.ActivityMainBinding
+import com.tst.bluetoothprint.util.Commands
 import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+    val printers: MutableList<BluetoothDevice> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.isEnabled.text = isBluetoothEnabled().toString()
+
+        binding.refresh.setOnClickListener {
+            printers.clear()
+            printers.addAll(connectedBluetoothPrinter())
+            binding.connected.text = printers.size.toString()
+        }
+
+        binding.print.setOnClickListener {
+            if(printers.isNotEmpty()){
+                val data: MutableList<PrintData> = ArrayList()
+                data.add(PrintData("HeadLine", align = Commands.TXT_ALIGN_CT, underline = Commands.TXT_UNDERL_ON))
+                data.add(PrintData("Line1", align = Commands.TXT_ALIGN_LT, underline = Commands.TXT_UNDERL_OFF))
+                data.add(PrintData("Line2"))
+                data.add(PrintData("Line3"))
+                data.add(PrintData("Line4"))
+
+                Thread(Runnable {
+                    print(printers[0], data)
+                }).start()
+            }
+        }
     }
 
-    private fun connectedBluetoothPrinter(): List<BluetoothDevice>? {
+    private fun isBluetoothEnabled() : Boolean{
+        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        return mBluetoothAdapter?.isEnabled ?: false
+    }
+
+    private fun requestEnableBluetooth(){
+        val enableBluetooth = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        startActivityForResult(enableBluetooth, 150)
+    }
+
+    private fun connectedBluetoothPrinter(): List<BluetoothDevice> {
         val printers: MutableList<BluetoothDevice> = ArrayList()
         try {
             val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
